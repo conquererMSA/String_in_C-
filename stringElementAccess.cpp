02ec4cfd86469b6fdf64603f406c8381e68ead8a
asus test <msa-99@outlook.com>
2024-05-26 15:41:52 +0600

    string element accessor

--- stringElementAccess.cpp
+++ stringElementAccess.cpp
@@ -1,16 +1,16 @@
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     string name;
-//     cin>>name;//cin space ney na.
-//     //ekhane string ekti class and dynamic array. string er size value assign korar sathe barte ba komte pare.
-//     //string ekti class tai eti ekti object variable return kore.
-//     //value assign na korle most case string er size 1 hoy.
+int main(){
+    string name;
+    cin>>name;//cin space ney na.
+    //ekhane string ekti class and dynamic array. string er size value assign korar sathe barte ba komte pare.
+    //string ekti class tai eti ekti object variable return kore.
+    //value assign na korle most case string er size 1 hoy.
 
-//     // element access related string function
-//     // name.at(index)=> return given index value.
-//     // name.back()=>return last index value.
+    // element access related string function
+    // name.at(index)=> return given index value.
+    // name.back()=>return last index value.
     
-//     return 0;
-// }
+    return 0;
+}
