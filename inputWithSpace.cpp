ea965e74bcc30db1ac4a9796b437262f5875d606
asus test <msa-99@outlook.com>
2024-05-26 15:40:12 +0600

    input taken with space

--- inputWithSpace.cpp
+++ inputWithSpace.cpp
@@ -1,14 +1,14 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     int n;
-//     string a;
-//     cin>>n;
-//     // cin.ignore();
-//     getchar();
-//     //getline tar ager 'enter' input niye ney, kintu porer 'enter' input ney na.
-//     getline(cin,a);
-//     cout<<n<<endl;
-//     cout<<a<<endl;
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    string a;
+    cin>>n;
+    // cin.ignore();
+    getchar();
+    //getline tar ager 'enter' input niye ney, kintu porer 'enter' input ney na.
+    getline(cin,a);
+    cout<<n<<endl;
+    cout<<a<<endl;
+    return 0;
+}
