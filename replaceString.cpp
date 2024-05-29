6e4ebef8889d49203c66046fee631b39b2d07193
asus test <msa-99@outlook.com>
2024-05-30 00:01:13 +0600

    explain question

---
+++ replaceString.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //AmirShaSalmanShaAtifShaKhanAmir
+    //ai string theke shudhumatro "Sha" bad diye "Sha" er jaygay space/" " bosiye dite hobe.
+    //output: Amir Salman Atif KhanAmir
+    return 0;
+}
