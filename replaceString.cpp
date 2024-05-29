d9a9a433b5ac0c24f34219feafaaea0b72017e9d
asus test <msa-99@outlook.com>
2024-05-30 00:09:11 +0600

    findout len

--- replaceString.cpp
+++ replaceString.cpp
@@ -4,5 +4,11 @@ int main(){
     //AmirShaSalmanShaAtifShaKhanAmir
     //ai string theke shudhumatro "Sha" bad diye "Sha" er jaygay space/" " bosiye dite hobe.
     //output: Amir Salman Atif KhanAmir
+    string s;
+    cin>>s;
+    string word="Sha";
+    int len=word.length();
+    
+
     return 0;
 }
