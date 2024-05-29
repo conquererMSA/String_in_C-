c323f5da781fc25afe38b4328888c65d231daba4
asus test <msa-99@outlook.com>
2024-05-29 23:56:41 +0600

    revweseWordOfSentence 2nd method

--- reverseWordOfSentence.cpp
+++ reverseWordOfSentence.cpp
@@ -6,13 +6,26 @@ int main(){
     //getout word one by one by using stringstream
     string word;
     stringstream ss(sen);
-    string reversWordSen;
+
+    //approch 1
+    // string reversWordSen;
+    // while(ss>>word){
+    //     //ekhon word ke kuno function ye diye reverse korte hobe.
+    //     reverse(word.begin(), word.end());
+    //     reversWordSen=reversWordSen+(" "+word);
+    // }
+    // cout<<reversWordSen.replace(0,1,"");
+
+    //approach 2
+    ss>>word;
+    reverse(word.begin(), word.end());
+    cout<<word;
     while(ss>>word){
-        //ekhon word ke kuno function ye diye reverse korte hobe.
-        
-        reverse(word.begin(), word.end());
-        reversWordSen=reversWordSen+(" "+word);
+        reverse(word.begin(),word.end());
+        cout<<" "<<word;
+        //evabe reverse korle proti word er pore space cole asche. ejonno sntence er prtom word manually stream theke ber kore nite hobe. erpor stream er 2nd word theke loop caliye reverse kore + reverse word er age space/" " add kore dilei problem solve hoye zabe
+
     }
-    cout<<reversWordSen.replace(0,1,"");
+
     return 0;
 }
