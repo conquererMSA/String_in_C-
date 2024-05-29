0343a278fa1fd978e73686b4d4468e606da32909
asus test <msa-99@outlook.com>
2024-05-30 00:27:14 +0600

    explanation of replacing

--- replaceString.cpp
+++ replaceString.cpp
@@ -13,7 +13,9 @@ int main(){
     // int index2=s.find("Tk");
     // cout<<index2;//-1
     while(s.find(word)!=-1){
+        //zotokkhon porzonto s e "Sha" er index pawya zacche totokkhon porzonto loop colte takbe.
         s.replace(s.find(word), len, " ");
+        //ekhane s asbe and protm "Sha" er index giye "Sha" erase hoye " " bosbe. porer loop e s hobe = Amir SalmanShaAtifShaKhanAmir, same abar erokom hobe and input s er 2nd "Sha" erase hoye " " bosbe, input s er 2nd "Sha" holeo 2nd loop er 1st "Sha" erase hobe evabe loop colte takbe and s e exist kora sob "Sha" erase hoye space bosbe. 
     }
     cout<<s;
     return 0;
